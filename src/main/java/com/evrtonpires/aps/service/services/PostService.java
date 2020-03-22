206bea73fafaf46f5f10f1e09c00fd7a05007965
evrtonpires <epe.masork@hotmail.com>
2020-03-22 20:34:02 -0300

    mudando valor para 'none'

--- src/main/java/com/evrtonpires/aps/service/services/PostService.java
+++ src/main/java/com/evrtonpires/aps/service/services/PostService.java
@@ -1,4 +1,5 @@
 package com.evrtonpires.aps.service.services;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,9 @@ public class PostService {
 
 	public PostDomain findById(Integer id) throws Exception {
 		Optional<PostDomain> obj = postRepository.findById(id);
-		return obj.orElseThrow(
-				() -> new Exception ("Nenhum Post encontrado referente ao ID: "+id)
-				);
+		return obj.orElseThrow(() -> new Exception("Nenhum Post encontrado referente ao ID: " + id));
 	}
-	
+
 	public PostDomain insertPost(PostDomain obj) {
 		obj.setId(null);
 		return postRepository.save(obj);
